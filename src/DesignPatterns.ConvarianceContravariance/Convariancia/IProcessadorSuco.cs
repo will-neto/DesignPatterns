23823080d9b5cf4d77ec677fcd5d9e75a13d0710
Wilson dos Santos Alberto <will.neto@outlook.com>
2024-09-16 20:46:24 -0300

    Covariancia

--- src/DesignPatterns.ConvarianceContravariance/Convariancia/IProcessadorSuco.cs
+++ src/DesignPatterns.ConvarianceContravariance/Convariancia/IProcessadorSuco.cs
@@ -9,5 +9,8 @@ namespace DesignPatterns.ConvarianceContravariance.Convariancia
     internal interface IProcessadorSuco<out T>
     {
         void FazerSuco();
+
+        // Não é possivel atribuir a covariancia em parametros de metodos, pois esse tipo de ação é usado em contravariancia
+        // void FazerSuco(T result); 
     }
 }
